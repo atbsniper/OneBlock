5c4b02550fc8085ee4b75305fb0cbd89d4cc2ff6
zainn021 <Sheamus1999@icloud.com>
2024-11-26 04:17:42 +0500

    app.js

--- backend/app.js
+++ backend/app.js
@@ -10,3 +10,16 @@ app.use(express.json());
 app.use("/LogGard", logs);
 
 module.exports = app;
+
+// sets up the Express application, routes for backend server
+//const express = require("express");
+//const logs = require("./Router/logs");
+//const cors = require('cors');
+
+//const app = express();
+//app.use(cors());
+//app.use(express.json());
+
+//app.use("/LogGard", logs);
+
+//module.exports = app;
