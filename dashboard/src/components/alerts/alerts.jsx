3c41a7492d066296de4136f3a75ef2eb10e3e9ba
atbsniper <124369451+atbsniper@users.noreply.github.com>
2024-12-09 17:04:08 +0500

    pagination removed

--- dashboard/src/components/alerts/alerts.jsx
+++ dashboard/src/components/alerts/alerts.jsx
@@ -240,15 +240,24 @@ function Alerts() {
                       {transactionHash && transactionHash[item.tokenId] == undefined
                         ? "-"
                         : transactionHash && transactionHash[item.tokenId]}
-                        </span>
-                      </p>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
+                    </span>
+                  </p>
+                </div>
+              );
+            })}
         </div>
-      );
-    }
+    {/*
+{alerts && alerts.length > alertsPerPage && (
+  <nav className="pagination-nav">
+    <ul className="pagination">
+      {renderPagination()}
+    </ul>
+  </nav>
+)}
+*/}
+      </div>
+    </div>
+  );
+}
 
 export default Alerts;
