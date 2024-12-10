b871eb8caaf3afdf78633e1e3f63f2aa77c676ea
atbsniper <124369451+atbsniper@users.noreply.github.com>
2024-12-10 20:52:56 +0500

    gcr like changes

--- studentportal/src/components/dashboard/dashboard.jsx
+++ studentportal/src/components/dashboard/dashboard.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./dashboard.css";
-import Banner from "../banner/Banner"; 
+import Banner from "../banner/Banner";
 
 const Dashboard = () => {
   const [selectedCourse, setSelectedCourse] = useState("");
   const navigate = useNavigate();
 
-  const handleCourseSelect = (course) => {
-    setSelectedCourse(course);
-  };
-
   const [loggedInUser, setLoggedInUser] = useState(null);
   useEffect(() => {
     setLoggedInUser(JSON.parse(localStorage.getItem("loggedInUser")));
   }, []);
-
-  useEffect(() => {
-    console.log(loggedInUser);
-  }, [loggedInUser]);
 
   const handleLogout = () => {
     setLoggedInUser(null);
@@ -29,36 +21,48 @@ const Dashboard = () => {
   return (
     <div>
       <div className="banner-container">
-        <Banner /> {/* Add the Banner component */}
+        <Banner />
       </div>
       <div className="dashboard">
-        <h2>Welcome {loggedInUser?.name}, Select a Course</h2>
-        <div className="course-boxes">
-          {loggedInUser?.courses?.map((course, index) => (
-            <div 
-              key={index} 
-              className="course-box"
-              onClick={() => handleCourseSelect(course.courseID)}
-            >
-              {course.courseName}
+        {!selectedCourse && (
+          <>
+            <h2>Welcome {loggedInUser?.name}, Select a Course</h2>
+            <div className="course-boxes">
+              {loggedInUser?.courses?.map((course, index) => (
+                <div
+                  key={index}
+                  className="course-box"
+                  onClick={() => setSelectedCourse(course.courseID)}
+                >
+                  {course.courseName}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        )}
 
         {selectedCourse && (
           <>
-            <h3>Selected Course: {selectedCourse}</h3>
+            <div className="selected-course">
+              Selected Course: {selectedCourse}
+            </div>
             <div className="action-boxes">
-              <div className="box" onClick={() => navigate(`/attendence/${selectedCourse}`)}>
+              <div
+                className="box"
+                onClick={() => navigate(`/attendence/${selectedCourse}`)}
+              >
                 Attendance
               </div>
-              <div className="box" onClick={() => navigate(`/grading/${selectedCourse}`)}>
+              <div
+                className="box"
+                onClick={() => navigate(`/grading/${selectedCourse}`)}
+              >
                 Grading
               </div>
             </div>
           </>
         )}
-        <div className="container logout-container">
+        <div className="logout-container">
           <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
